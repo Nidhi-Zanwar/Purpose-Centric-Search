495186a6111aa8f765cc1aaaf2573b83c9e2c911
Nidhi-Zanwar <nidhizanwar6@gmail.com>
2013-04-13 17:06:00 +0530

    Create PurposeCentricSearch.i18.php

---
+++ PurposeCentricSearch.i18.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Internationalization file for the PurposeCentricSearch extension.
+ */
+
+$messages = array();
+
+/** English*/
+$messages['en'] = array(
+  'PurposeCentricSearch' => 'Purpose Centric Search',
+	'PurposeCentricSearch-SearchQuery' => 'Search Query : ',
+	'PurposeCentricSearch-submit' => 'search',
+	'PurposeCentricSearch-infobox' =>'Infobox',
+	'PurposeCentricSearch-searchin-title' => 'SearchTitle',
+	'PurposeCentricSearch-searchin-content' => 'SearchContent',
+	'PurposeCentricSearch-selectall' => 'Select all',
+	'PurposeCentricSearch-selectnone' => 'Select none',
+	'purposecentricsearch-invertselection'=> 'Invert Select',
+	'PurposeCentricSearch-namespaces' => 'Namespaces',
+	'PurposeCentricSearch-PurposeExpertise' => 'Purpose and Expertise',
+	'level-dropdown'=> 'Intermediate
+						Expert',
+	'conLevel-dropdown'=>'Intermediate
+						  Expert',
+	'PurposeCentricSearch-error1' => 'Search query is null.Please enter a valid query',
+	'PurposeCentricSearch-error2' => 'Select atleast one - search in Title/search in Content',
+	'PurposeCentricSearch-error3' => 'No namespace selected.Please select namespace',
+	'PurposeCentricSearch-noResultMessage' => 'No result found',
+	'purpose-dropdown' => 'Produce
+							Maintain',
+	'Page-Expertise' => '<b>Expertise : </b>',
+	'Unreviewed Page' => 'Unreviewed Page',
+	'Expertise'=> '  Expertise : ',		
+	'Purpose' => '  Purpose : ',
+	'empty' =>'Inappropriate Search Query',
+	'lessContent'=>'This page has less content.',
+	'Irrelevant'=> 'Less relevant Results',
+	'PurposeCentricSearch-missing'=>'Missing Files: ',
+	'PurposeCentricSearch-more' => '...',
+	'PurposeCentricSearch-UserContext' => 'User Specific Details ( Project details, work domain etc.)',
+	'Search-Results'=>'Search Results',
+	'PurposeCentricSearch-unused-Produce' => 'These files are unused and can be integrated with related pages',
+	'PurposeCentricSearch-unused-Maintain' => 'These files are unused and can be considered for deletion',
+	'PurposeCentricSearch-Pending' => 'This page has been modified and changes are yet to be approved.',
+	'PurposeCentricSearch-PageNew' => 'This page is New and not approved yet.',
+	'PurposeCentricSearch-infobox' => 'Infobox',
+	'PurposeCentricSearch-page-level'=> 'Expertise:',
+	'PurposeCentricSearch-Wanted' => 'These pages are Wanted but do not exist',
+	'PurposeCentricSearch-pageWordMatched' => 'Word Matched :',
+	'PurposeCentricSearch-page-size' => 'Page size :',
+	'PurposeCentricSearch-Byte' => ' Bytes',
+	'PurposeCentricSearch-page-vote'=> 'Usability',
+	'percentage'=>'%',
+	'PurposeCentricSearch-modification-count'=> '<b>Modification Count :</b>',
+	'PurposeCentricSearch-page-modification-date'=> 'Last Modified :',
+	'PurposeCentricSearch-Broken-redirects' =>'This page redirects to a page that does not exist',
+	'PurposeCentricSearch-Lonely'=>'This page is not being referred by any other page',
+	'PurposeCentricSearch-deadEnd'=>'This page does not refer to any other page',
+	'PurposeCentricSearch-Short' =>'Short',
+		
+	
+);
