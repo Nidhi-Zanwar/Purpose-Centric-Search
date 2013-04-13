c36fe5486aae89f72bb9488459788f6cd9d19945
Nidhi-Zanwar <nidhizanwar6@gmail.com>
2013-04-13 17:08:54 +0530

    Create PurposeCentricSearch.alias.php

---
+++ PurposeCentricSearch.alias.php
@@ -0,0 +1,8 @@
+<?php
+/* Aliases for PurposeCentricSearch */
+$specialPageAliases = array();
+
+/** English */
+$specialPageAliases['en'] = array(
+  'PurposeCentricSearch' => array( 'PurposeCentricSearch' )
+);
